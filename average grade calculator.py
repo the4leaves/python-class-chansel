5b47d59b30aded4d4f279c96f731480cab1b14c3
the4leaves <68232752+the4leaves@users.noreply.github.com>
2020-07-14 11:35:47 -0400

    Create average grade calculator.py

---
+++ average grade calculator.py
@@ -0,0 +1,69 @@
+getinput = False
+grade = 0
+gradestotal = 0
+x = 2
+getinput = False
+z = 0
+while getinput is False:
+    try:
+        while x == 2:
+                grade = grade + float(input('input grade percentage... '))
+                x = int(input('if done type 1 if not done type 2...  '))
+                getinput = True
+                gradestotal += 1
+        y = grade / gradestotal
+        print ('Your average grade is ' + str(y) + ' percent')
+    except ValueError:
+        print ('you must put in percentages WITHOUT the percent sign')
+while z = 0   
+    if y >= 97:
+        print ('thats an A+!')
+        z += 1
+while z = 0
+    if y >= 94:
+        print ('thats an A!')
+        z += 1
+while z = 0
+    if y >= 90:
+        print ('thats an A-!')
+        z += 1
+while z = 0
+    if y >= 87:
+        print ('thats an B+!')
+        z += 1
+while z = 0
+    if y >= 84:
+        print ('thats an B!')
+        z += 1
+while z = 0
+    if y >= 80:
+        print ('thats an B-!')
+        z += 1
+while z = 0
+    if y >= 77:
+        print ('thats an C+!')
+        z += 1
+while z = 0
+    if y >= 74:
+        print ('thats an C!')
+        z += 1
+while z = 0
+    if y >= 70:
+        print ('thats an C-!')
+        z += 1
+while z = 0
+    if y >= 67:
+        print ('thats an D+!')
+        z += 1
+while z = 0
+    if y >= 64:
+        print ('thats an D!')
+        z += 1
+while z = 0
+    if y >= 60:
+        print ('thats an D-!')
+        z += 1
+while z = 0
+    if y >= 0:
+        print ('thats an F!')
+        z += 1
