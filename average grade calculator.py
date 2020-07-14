b13986c838532f3f46dbccdaf50d872384ac42d4
the4leaves <68232752+the4leaves@users.noreply.github.com>
2020-07-14 12:47:47 -0400

    completed calculator

--- average grade calculator.py
+++ average grade calculator.py
@@ -1,69 +1,47 @@
-getinput = False
-grade = 0
 gradestotal = 0
-x = 2
-getinput = False
+y = 0
 z = 0
-while getinput is False:
+grade = 0
+while True:
+    tempgrade = input('input grade percentage... ')
     try:
-        while x == 2:
-                grade = grade + float(input('input grade percentage... '))
-                x = int(input('if done type 1 if not done type 2...  '))
-                getinput = True
-                gradestotal += 1
-        y = grade / gradestotal
-        print ('Your average grade is ' + str(y) + ' percent')
-    except ValueError:
-        print ('you must put in percentages WITHOUT the percent sign')
-while z = 0   
-    if y >= 97:
-        print ('thats an A+!')
-        z += 1
-while z = 0
-    if y >= 94:
-        print ('thats an A!')
-        z += 1
-while z = 0
-    if y >= 90:
-        print ('thats an A-!')
-        z += 1
-while z = 0
-    if y >= 87:
-        print ('thats an B+!')
-        z += 1
-while z = 0
-    if y >= 84:
-        print ('thats an B!')
-        z += 1
-while z = 0
-    if y >= 80:
-        print ('thats an B-!')
-        z += 1
-while z = 0
-    if y >= 77:
-        print ('thats an C+!')
-        z += 1
-while z = 0
-    if y >= 74:
-        print ('thats an C!')
-        z += 1
-while z = 0
-    if y >= 70:
-        print ('thats an C-!')
-        z += 1
-while z = 0
-    if y >= 67:
-        print ('thats an D+!')
-        z += 1
-while z = 0
-    if y >= 64:
-        print ('thats an D!')
-        z += 1
-while z = 0
-    if y >= 60:
-        print ('thats an D-!')
-        z += 1
-while z = 0
-    if y >= 0:
-        print ('thats an F!')
-        z += 1
+        tempgrade = int(tempgrade)
+    except:
+        if tempgrade == 'stop':
+            break
+        else:
+            print('type in stop or a grade percent without the sign... ')
+    gradestotal += 1
+    grade += tempgrade
+y = grade / gradestotal
+y = round(y)
+print ('Your average grade is ' + str(y) + ' percent')
+
+if y >= 97:
+    print ('thats an A+!')
+elif y >= 94:
+    print ('thats an A!')
+elif y >= 90:
+    print ('thats an A-!')
+elif y >= 87:
+    print ('thats an B+!')
+elif y >= 84:
+    print ('thats an B!')
+elif y >= 80:
+    print ('thats an B-!')
+elif y >= 77:
+    print ('thats an C+!')
+elif y >= 74:
+    print ('thats an C!')
+elif y >= 70:
+    print ('thats an C-!')
+elif y >= 67:
+    print ('thats an D+!')
+elif y >= 64:
+    print ('thats an D!')
+elif y >= 60:
+    print ('thats an D-!')
+else:
+    print ('thats an F!')
+       
+input('press any key to quit... ')
